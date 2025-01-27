a5be7473d25b2fe0e22553545db60ee07702353f
diandraistazar <diandraistazar@gmail.com>
2025-01-27 22:40:18 +0700

    Latihan 5 Arrow Function

--- JavaScript/Pemrograman Dasar dengan JavaScript/Latihan 20 JavaScript/javascript.js
+++ JavaScript/Pemrograman Dasar dengan JavaScript/Latihan 20 JavaScript/javascript.js
@@ -8,7 +8,6 @@ var nama = ["Diandra", "Mita", "Kind Mita", "Cappie"];
 nama.forEach(function(name) {
     console.info("Halo, nama saya " + name)
 })
-
 // Semacam melakukan fungsi ke setiap element yang berada didalam Array, bisa di lihat aku melakukan console.info ke setiap element dalam Array nama. Jadi pengulangna ini akan berhenti jika elementnya selesai atau mencapai akhir.
 
 var angka = [1, 2, 3];
@@ -17,11 +16,8 @@ var hasil = 0;
 angka.forEach(function(angka) {
     hasil += angka
 })
-
 console.info(hasil)
-
 // Hasil dari method tersebut adalah 6, aku menambahkan 1 + 2 + 3 = 6
-
 // forEach memberikan 3 argument kepada function, yaitu element, index, dan array
 
 
@@ -31,8 +27,8 @@ var hitung = [5,10,2];
 var hasil = hitung.map(function(hitung) {
     return hitung * 2;
 })
-
 console.info(hasil)
+// Method Map ini berfungsi untuk melakukan sesuatu pada setiap element array setelah itu element tersebut tidak di kembalikan ke array yang asli melainkan dikembalikan dalam bentuk array yang baru, array yang baru disini adalah hasil, jadi array hitung tidak akan terpengaruh apa-apa
 
 
 
@@ -41,8 +37,6 @@ var angka5 = [1,30,,3,2,50,54];
 angka5.sort(function(a,b) {
     return a-b
 })
-
 console.info(angka5)
-
 // Berfungsi untuk mengurutkan element Array
 // a-b disini bukan pengurangan melainkan pengurutan dalam sort()
